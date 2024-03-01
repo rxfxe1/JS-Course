76f5157bcf0482dc453fc88617257cea3ff24cfe
Estágio Infor <EST.INFOR@pcruz.pt>
2024-03-01 16:26:28 +0000

    Vínculos de Atributo (v-bind)

---
+++ Vue.js/Tutorial/v-bind.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const titleClass = ref('title')
+</script>
+
+<!--template>
+    <h1>Make me red</h1>
+</template-->
+
+<!--  adicionei um vinculo de class dinamico ao h1 -->
+
+<template>
+  <h1 :class="titleClass">Make me red</h1>
+</template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
