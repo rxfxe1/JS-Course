fefedccb6ae982313165d6446a449b314b349e1c
Estágio Infor <EST.INFOR@pcruz.pt>
2024-03-04 10:36:17 +0000

    Practice Assignement

--- Course/Fundamentals - Pt. 1/script.js
+++ Course/Fundamentals - Pt. 1/script.js
@@ -1,4 +1,16 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
+//      LINKING JS FILE 
+// let js = 'amazing';
+// console.log(40+90-50);
 
-console.log(40+90-50);
+//      VALUES AND VARIABLES
+// console.log("Rafael");
+
+// let firstName = "Rafael";
+// console.log(firstName);
+
+//      PRACTICE ASSIGNMENT
+let country = "Portugal";
+let continent = "Europe";
+let population = 11000000;
+
+console.log("My coutry is ", {country}, " in ", {continent}, " with a population of", {population});
