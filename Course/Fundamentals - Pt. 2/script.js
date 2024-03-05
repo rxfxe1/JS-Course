ae97de0f0f9631296e6656cb29c05aa117b4bfa4
Estágio Infor <EST.INFOR@pcruz.pt>
2024-03-05 12:40:11 +0000

    Strict Mode Functioning

---
+++ Course/Fundamentals - Pt. 2/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive.");
+
+// const interface = "Audio";
+// const private = "Private";
+// const if = 23;
+// some variable names are reserved for future features or forbidden names
