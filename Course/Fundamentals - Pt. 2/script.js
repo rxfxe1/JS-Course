3d3190aaa901109c509fc48af94e974ce5a88140
Estágio Infor <EST.INFOR@pcruz.pt>
2024-03-05 15:23:42 +0000

    Function Declarations vs Expressions

--- Course/Fundamentals - Pt. 2/script.js
+++ Course/Fundamentals - Pt. 2/script.js
@@ -38,3 +38,31 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 */
+
+
+//      FUNCTION DECLARATIONS VS EXPRESSIONS
+
+/*
+// Function Declaration
+
+// function calcAge1(birthYear) {
+//     const age = 2024 - birthYear;
+//     return age;
+// }
+
+// we can return everything in one go like this
+function calcAge1(birthYear) {
+    return 2024 - birthYear;
+}
+const age1 = calcAge1(2004);
+
+
+// Functions Expression
+const calcAge2 = function (birthYear) {
+    return 2024 - birthYear;
+}
+const age2 = calcAge2(2004);
+
+
+console.log(age1, age2);
+*/
