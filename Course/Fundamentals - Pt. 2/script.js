cf1eb547a101803fc2b020bd170ae548b3690954
Estágio Infor <EST.INFOR@pcruz.pt>
2024-03-05 17:00:39 +0000

    Functions Review

--- Course/Fundamentals - Pt. 2/script.js
+++ Course/Fundamentals - Pt. 2/script.js
@@ -105,3 +105,27 @@ function fruitProcessor(apples, oranges) {
 }
 console.log(fruitProcessor(2, 3));
 */
+
+
+//      FUNCTIONS REVIEW
+
+const calcAge = function (birthYear) {
+    return 2024 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`);
+        return -1;
+    }
+}
+
+console.log(yearsUntilRetirement(2004, "Rafael"));
+console.log(yearsUntilRetirement(1991, "Jonas"));
+
