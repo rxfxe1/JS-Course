3b654c12e85cb9997159297890787b7b782ff8ed
Estágio Infor <EST.INFOR@pcruz.pt>
2024-03-05 14:52:06 +0000

    Function Fundamentals

--- Course/Fundamentals - Pt. 2/script.js
+++ Course/Fundamentals - Pt. 2/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//      STRICT MODE
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -10,3 +12,29 @@ if (hasDriversLicense) console.log("I can drive.");
 // const private = "Private";
 // const if = 23;
 // some variable names are reserved for future features or forbidden names
+*/
+
+
+//      FUNCTION FUNDAMENTALS
+/*
+function logger() {
+    console.log("My name is Rafael.");
+}
+
+// calling, running or invoking the function
+logger();
+// logger(); // as many times as we want
+// logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0); // values that are assigned to the parameters defined earlier
+console.log(appleJuice);
+
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
