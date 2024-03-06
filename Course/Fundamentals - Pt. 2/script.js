089284c566f03e033ea64ddd4aebc8ded6990278
Estágio Infor <EST.INFOR@pcruz.pt>
2024-03-06 12:59:02 +0000

    Basic Array Operators (Methods)

--- Course/Fundamentals - Pt. 2/script.js
+++ Course/Fundamentals - Pt. 2/script.js
@@ -165,7 +165,7 @@ const rafael = [firstName, "Silva", 2024 - 2004, "Student", friends];
 console.log(rafael);
 
 
-// Exercise 
+// Exercise
 const calcAge = function (birthYear) {
     return 2024 - birthYear;
 }
@@ -186,3 +186,61 @@ const ages = [calcAge(year[0]), calcAge(year[1]), calcAge(year[year.length - 1])
 
 console.log(ages);
 */
+
+
+//      BASIC ARRAY OPERATIONS (METHODS)
+
+// push method
+const friends = ["Michael", "Sarah", "John"];
+// friends.push("Jay");
+// this method pushes a new value but also returns it's length if we store it in a variable
+const newLength = friends.push("Jay");
+console.log(newLength);
+
+
+// unshift method
+friends.unshift("Mark");
+console.log(friends);
+// this method adds a new element at the beginning of the array
+// it also returns the length if necessary
+
+
+// pop method
+friends.pop();
+console.log(friends);
+// this method removes the last element of the array
+// this method returns the removed element
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+
+// shift method
+friends.shift();
+console.log(friends);
+// this methods removes the first element of the array
+// this method also returns the element that was removed
+
+
+// indexOf method
+console.log(friends.indexOf("Michael"));
+// this method returns the index at which the element is located
+// if we try with a non-existent element, we get -1
+
+
+// includes method
+console.log(friends.includes("Sarah"));
+console.log(friends.includes("Bob"));
+//this method returns a boolean value to tell us
+// if the element we're looking for is present in the array
+
+// this method uses strict equality to check
+friends.push(23);
+console.log(friends.includes("23")); // false
+console.log(friends.includes(23)); // true
+
+// we can use this method to write conditionals
+if (friends.includes("Michael")) { // since this returns a boolean
+    console.log("You have a friend called Michael");
+}
+
