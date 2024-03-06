2b5b26451ce40e4231622b4112d86607d3b41ab9
Estágio Infor <EST.INFOR@pcruz.pt>
2024-03-06 09:20:51 +0000

    Challenge #1 (success)

--- Course/Fundamentals - Pt. 2/challenges/challenge1.js
+++ Course/Fundamentals - Pt. 2/challenges/challenge1.js
@@ -1,26 +1,48 @@
-let calcAverage = (score1, score2, score3) => {
-    let scoreAverage = (score1 + score2 + score3) / 3;
-    return scoreAverage;
-}
+"use strict"
 
+const calcAverage = (score1, score2, score3) => { // since it's a one-liner, no need to return
+    return (score1 + score2 + score3) / 3;
+};
 
-scoreDolphins = calcAverage(44, 23, 7);
-console.log(scoreDolphins);
-scoreKoalas = calcAverage(85, 54, 40);
-console.log(scoreDolphins);
+const scoreDolphins = calcAverage(85, 54, 41); // const or let ?
+const scoreKoalas = calcAverage(23, 34, 27);
 
-const checkWinner = function (avgDolphins, avgKoalas) {
-    avgDolphins = scoreDolphins;
-    avgKoalas = scoreKoalas;
-    
+const checkWinner = function() {
+    const avgDolphins = scoreDolphins;
+    const avgKoalas = scoreKoalas;
+    console.log(avgDolphins, avgKoalas);
+
     if (avgDolphins > avgKoalas && avgDolphins >= (avgKoalas * 2)) {
         console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
     } else if (avgKoalas > avgDolphins && avgKoalas >= (avgDolphins * 2)) {
         console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
     } else {
-        console.log("No team wins...");
+        console.log("No team wins...")
     }
-    return checkWinner;
-}
+};
+
+checkWinner();
+
+
+//      CORRECTION
+
+/*
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
  
-console.log(checkWinner);
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+ 
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log('No team wins...');
+  }
+};
+ 
+checkWinner(scoreDolphins, scoreKoalas);
+*/
