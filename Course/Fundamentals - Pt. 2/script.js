c5c68bdea68d77000205f528f59b1b8dbfcdb711
Estágio Infor <EST.INFOR@pcruz.pt>
2024-03-06 12:23:52 +0000

    Introduction to Arrays

--- Course/Fundamentals - Pt. 2/script.js
+++ Course/Fundamentals - Pt. 2/script.js
@@ -109,6 +109,7 @@ console.log(fruitProcessor(2, 3));
 
 //      FUNCTIONS REVIEW
 
+/*
 const calcAge = function (birthYear) {
     return 2024 - birthYear;
 }
@@ -116,7 +117,7 @@ const calcAge = function (birthYear) {
 const yearsUntilRetirement = function (birthYear, firstName) {
     const age = calcAge(birthYear);
     const retirement = 65 - age;
-    
+
     if (retirement > 0) {
         console.log(`${firstName} retires in ${retirement} years`);
         return retirement;
@@ -128,4 +129,60 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement(2004, "Rafael"));
 console.log(yearsUntilRetirement(1991, "Jonas"));
+*/
 
+
+//       INTRODUCTION TO ARRAYS
+
+/*
+// instead of doing this
+const friend1 = "Michael";
+const friend2 = "Sarah";
+const friend3 = "John";
+
+// we do this
+const friends = ["Michael", "Sarah", "John"];
+console.log(friends);
+
+// we can also do it like this
+const years = new Array(1999, 2002, 2004, 2024);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]); // gives the last element
+
+friends[2] = "Juan";
+console.log(friends);
+
+// friends = ["Bob", "Alice"]; // not possible, we can only change primitive values
+
+
+// we can store different types of values in the same array
+const firstName = "Rafael";
+const rafael = [firstName, "Silva", 2024 - 2004, "Student", friends];
+console.log(rafael);
+
+
+// Exercise 
+const calcAge = function (birthYear) {
+    return 2024 - birthYear;
+}
+const year = [1977, 1982, 2002, 2004, 2010];
+
+// if we wanted to calculate the age of all the values in the array
+// we could not do this
+// console.log(calcAge(year)); // does not work because is an array and not a single value
+
+//what we can do is
+const age1 = calcAge(year[0]); // first element
+const age2 = calcAge(year[1]); // second element
+const age3 = calcAge(year[year.length - 1]); // last element
+console.log(age1, age2, age3);
+
+// same thing with an Array
+const ages = [calcAge(year[0]), calcAge(year[1]), calcAge(year[year.length - 1])];
+
+console.log(ages);
+*/
