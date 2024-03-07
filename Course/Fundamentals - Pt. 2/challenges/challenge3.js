7e5389b4cf3a940946ab9749e1025d8ff2f1e457
Estágio Infor <EST.INFOR@pcruz.pt>
2024-03-07 09:05:10 +0000

    Challenge #3 (success)

---
+++ Course/Fundamentals - Pt. 2/challenges/challenge3.js
@@ -0,0 +1,27 @@
+// BMI = mass / /height * height)
+
+const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height); // (height ** 2)
+        return this.bmi;
+    }
+};
+
+const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+};
+
+if (mark.calcBMI() > john.calcBMI()) {
+    console.log(`${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${john.fullName}'s (${john.calcBMI()})!`)
+} else {
+    console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${mark.fullName}'s (${mark.calcBMI()})!`)
+}
