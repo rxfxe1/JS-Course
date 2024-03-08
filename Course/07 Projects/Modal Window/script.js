920d4a78a954c9956b64d4c84bde4c22670724d9
Rafael Silva <rafaelfilipe16@hotmail.com>
2024-03-08 14:44:44 +0000

    Esc key functionality

--- Course/07 Projects/Modal Window/script.js
+++ Course/07 Projects/Modal Window/script.js
@@ -21,3 +21,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) btnsOpenModal[i].addEventListener
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
