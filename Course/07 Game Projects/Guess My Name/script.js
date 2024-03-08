5b709ea1c0237e720b63ae127ea607d0d8d193ef
Rafael Silva <rafaelfilipe16@hotmail.com>
2024-03-08 11:19:49 +0000

    When player wins

--- Course/07 Game Projects/Guess My Name/script.js
+++ Course/07 Game Projects/Guess My Name/script.js
@@ -14,6 +14,7 @@ console.log(document.querySelector('.guess').value); // empty bc nothing there
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -24,5 +25,13 @@ document.querySelector('.check').addEventListener('click', function () {
   // !guess = !false = true (since true => executes code)
   if (!guess) {
     document.querySelector('.message').textContent = '🚫 No Number!';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   }
 });
