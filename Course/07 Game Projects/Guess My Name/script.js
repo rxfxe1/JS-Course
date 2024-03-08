2bb1dd96492158012e1e7419a99890eb7b17c879
Rafael Silva <rafaelfilipe16@hotmail.com>
2024-03-08 11:17:47 +0000

    Show message when there's no input

--- Course/07 Game Projects/Guess My Name/script.js
+++ Course/07 Game Projects/Guess My Name/script.js
@@ -11,3 +11,18 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value); // empty bc nothing there
 */
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there's no input
+  // if no input, guess = 0 (no number) = false
+  // !guess = !false = true (since true => executes code)
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No Number!';
+  }
+});
