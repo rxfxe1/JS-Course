7769b9457ce30d4dba1dd8a18347f57f4baea634
Rafael Silva <rafaelfilipe16@hotmail.com>
2024-03-11 14:16:24 +0000

    Exercises

--- Course/09 Data Structures, Modern Operators and Strings/exercises/exercises.js
+++ Course/09 Data Structures, Modern Operators and Strings/exercises/exercises.js
@@ -192,6 +192,35 @@ const books = [
   },
 ];
 
+// Destructuring Objects
+
+// 2.1
+const { title, author, ISBN } = books[0];
+
+// 2.2
+const { keywords: tags } = books[0];
+
+// 2.3
+const { language, programmingLanguage = 'unknown' } = books[6];
+
+// 2.4
+let bookTitle = 'unknown';
+let bookAuthor = 'unknown';
+({ title: bookTitle, author: bookAuthor } = books[0]);
+
+// 2.5
+const {
+  thirdParty: {
+    goodreads: { rating: bookRating },
+  },
+} = books[0];
+
+// 2.6
+function printBookInfo({ title, author, year = 'year unknown' }) {
+  console.log(`${title} by ${author}, ${year}`);
+}
+printBookInfo({ title: 'Algorithms', author: 'Robert Sedgewick', year: '2011' });
+
 //      Destructuring Arrays
 
 //  1.1
